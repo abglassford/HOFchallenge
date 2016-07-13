f9aa01461123e245a1447a510a9b2c2c7b7d721b
Alexander Glassford <a.b.glassford@gmail.com>
2016-07-12 22:44:51 -0600

    complete 3 of 4 exercises

---
+++ challenge.js
@@ -0,0 +1,48 @@
+console.log(greaterThan10(11)); //true
+console.log(greaterThan10(3)); //false
+console.log(any([8, 9, 10, 11], greaterThan10)); //[false, false, false, true]
+console.log(surprise('surprise')('!')); //'surprise!'
+console.log(onlyOdd(3)); //3
+console.log(onlyOdd(2)); //false
+console.log(filter([ 1, 2, 3, 4 ], onlyOdd));
+
+
+function greaterThan10(num){
+  if(!Number(num) || num <= 10){
+    return false
+  }else{
+    return true
+  }
+}
+
+function any(nums, greaterThan10Fn){
+  var result = []
+  for(var i = 0; i < nums.length; i++){
+    result.push(greaterThan10Fn(nums[i]))
+  }
+  return result;
+}
+
+function surprise(word){
+  return function (punctuation){
+    return word + punctuation
+  }
+}
+
+function onlyOdd(num){
+  if(num % 2 === 0 || !Number(num)){
+    return false
+  } else {
+    return true
+  }
+}
+
+function filter(nums, onlyOddFn){
+  var oddArr = []
+  for(var i = 0; i < nums.length; i++){
+    if(onlyOddFn(nums[i]) === true){
+      oddArr.push(nums[i])
+    }
+  }
+  return oddArr
+}
